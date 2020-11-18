cdc8d3ff9ddba62601c433daebd4842a11dbea93
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-11-18 12:25:40 +0200

    Move particles while the mousewheel

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import lion from './img/lion.png';
 export default class Sketch {
   constructor() {
     this.time = 0;
+    this.move = 0;
     this.renderer = new THREE.WebGLRenderer( { antialias: true } );
     this.renderer.setSize( window.innerWidth, window.innerHeight );
     document.getElementById('container').appendChild( this.renderer.domElement );
@@ -23,9 +24,16 @@ export default class Sketch {
       new THREE.TextureLoader().load(lion),
     ];
     this.mask = new THREE.TextureLoader().load(gradient),
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    //this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.addMesh();
+    this.mouseEffects();
     this.render();
+  }
+
+  mouseEffects() {
+    window.addEventListener('mousewheel', (e) => {
+      this.move += e.wheelDeltaY/1000;
+    })
   }
   
   addMesh() {
@@ -37,6 +45,8 @@ export default class Sketch {
         cat: { type: "t", value: this.textures[0] },
         lion: { type: "t", value: this.textures[1] },
         mask: { type: "t", value: this.mask },
+        move: { type: "t", value: 0 },
+        time: { type: "t", value: 0 },
       },
       side: THREE.DoubleSide,
       transparent: true,
@@ -49,20 +59,30 @@ export default class Sketch {
     this.geometry = new THREE.BufferGeometry();
     this.positions = new THREE.BufferAttribute(new Float32Array(number*3), 3);
     this.coordinates = new THREE.BufferAttribute(new Float32Array(number*3), 3);
+    this.speeds = new THREE.BufferAttribute(new Float32Array(number), 1);
+    this.offset = new THREE.BufferAttribute(new Float32Array(number), 1);
 
     let index = 0;
+
+    function rand(a, b) {
+      return a + (b - a)*Math.random();
+    }
 
     for (let i = 0; i < 512; i++) {
       let posX = i - 256;
       for (let j = 0; j < 512; j++) {
         this.positions.setXYZ(index, posX*2, (j - 256)*2, 0);
         this.coordinates.setXYZ(index, i, j, 0);
+        this.offset.setX(index, rand(-1000, 1000));
+        this.speeds.setX(index, rand(0.4, 1));
         index++;
       }
     }
 
     this.geometry.setAttribute("position", this.positions);
     this.geometry.setAttribute("aCoordinates", this.coordinates);
+    this.geometry.setAttribute("aOffset", this.offset);
+    this.geometry.setAttribute("aSpeed", this.speeds);
 
     this.mesh = new THREE.Points( this.geometry, this.material );
 	  this.scene.add( this.mesh );
@@ -72,7 +92,8 @@ export default class Sketch {
     this.time++;
     // this.mesh.rotation.x += 0.01;
     // this.mesh.rotation.y += 0.02;
-    
+    this.material.uniforms.time.value = this.time;
+    this.material.uniforms.move.value = this.move;
     this.renderer.render( this.scene, this.camera );
     window.requestAnimationFrame(this.render.bind(this));
   }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,10 +1,18 @@
 varying vec2 vUv;
 varying vec2 vCoordinates;
 attribute vec3 aCoordinates;
+attribute float aSpeed;
+attribute float aOffset;
+
+uniform float move;
+uniform float time;
 
 void main() {
   vUv = uv;
-  vec4 mvPosition = modelViewMatrix * vec4( position, 1. );
+  vec3 pos = position;
+  pos.z = position.z + (move * 2000. * aSpeed) + aOffset;
+
+  vec4 mvPosition = modelViewMatrix * vec4( pos, 1. );
   gl_PointSize = 1000. * ( 1. / - mvPosition.z );
   gl_Position = projectionMatrix * mvPosition;
 
